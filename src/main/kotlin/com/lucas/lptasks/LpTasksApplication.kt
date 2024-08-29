d4882db39eec8384c5dc4267a0f28484eebb4571
LukasPio <lucaspio.galvao@gmail.com>
2024-08-28 23:41:43 -0300

    initial commit

---
+++ src/main/kotlin/com/lucas/lptasks/LpTasksApplication.kt
@@ -0,0 +1,11 @@
+package com.lucas.lptasks
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LpTasksApplication
+
+fun main(args: Array<String>) {
+    runApplication<LpTasksApplication>(*args)
+}
