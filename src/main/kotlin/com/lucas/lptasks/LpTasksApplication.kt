9ab50801da196e79b62c20469760cfead41450d9
LukasPio <lucaspio.galvao@gmail.com>
2024-08-29 23:37:58 -0300

    chore(cache): enable caching

--- src/main/kotlin/com/lucas/lptasks/LpTasksApplication.kt
+++ src/main/kotlin/com/lucas/lptasks/LpTasksApplication.kt
@@ -2,10 +2,11 @@ package com.lucas.lptasks
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class LpTasksApplication
-
 fun main(args: Array<String>) {
     runApplication<LpTasksApplication>(*args)
 }
