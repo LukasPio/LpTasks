84102c20e4cd4e9b37d81b31b80cc9e3b47121b9
Lucas Galvao <lucaspio.galvao@gmail.com>
2024-08-31 19:50:57 -0300

    feat(repository): create method to check if task exists by id

--- src/main/kotlin/com/lucas/lptasks/repository/TaskRepository.kt
+++ src/main/kotlin/com/lucas/lptasks/repository/TaskRepository.kt
@@ -4,4 +4,6 @@ import com.lucas.lptasks.model.Task
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
-interface TaskRepository : JpaRepository<Task, UUID>
+interface TaskRepository : JpaRepository<Task, UUID> {
+    fun existsTaskById(taskId: UUID): Boolean
+}
