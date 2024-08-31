87202e733f3e5c65ab4388a1c7f743d57d4c4596
Lucas Galvao <lucaspio.galvao@gmail.com>
2024-08-31 14:55:20 -0300

    feat(repository): create repository to task

---
+++ src/main/kotlin/com/lucas/lptasks/repository/TaskRepository.kt
@@ -0,0 +1,7 @@
+package com.lucas.lptasks.repository
+
+import com.lucas.lptasks.model.Task
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface TaskRepository : JpaRepository<Task, UUID>
