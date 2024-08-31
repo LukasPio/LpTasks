4d27959294c34de1a4cce78d27d7d7d8123e0c58
Lucas Galvao <lucaspio.galvao@gmail.com>
2024-08-31 14:55:08 -0300

    feat(controller): create a controller to tasks

---
+++ src/main/kotlin/com/lucas/lptasks/controller/TaskController.kt
@@ -0,0 +1,30 @@
+package com.lucas.lptasks.controller
+
+import com.lucas.lptasks.dto.TaskRequestDTO
+import com.lucas.lptasks.service.TaskService
+import com.lucas.lptasks.utils.ApiResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/app/tasks")
+class TaskController(
+    private val taskService: TaskService,
+) {
+    @GetMapping
+    fun getTasks(): ApiResponse<out Any?> = taskService.getAllTasks()
+
+    @GetMapping("/{id}")
+    fun getTaskById(
+        @PathVariable id: String,
+    ): ApiResponse<out Any?> = taskService.getTaskById(id)
+
+    @PostMapping
+    fun saveTask(
+        @RequestBody taskData: TaskRequestDTO,
+    ): ApiResponse<out Any?> = taskService.saveTask(taskData)
+}
