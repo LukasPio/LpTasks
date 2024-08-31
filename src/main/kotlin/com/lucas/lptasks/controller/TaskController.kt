0aaa4abd2e4acb06db063b5ad71def8863d17123
Lucas Galvao <lucaspio.galvao@gmail.com>
2024-08-31 19:49:52 -0300

    feat(controller): create route to save new tasks

--- src/main/kotlin/com/lucas/lptasks/controller/TaskController.kt
+++ src/main/kotlin/com/lucas/lptasks/controller/TaskController.kt
@@ -3,6 +3,7 @@ package com.lucas.lptasks.controller
 import com.lucas.lptasks.dto.TaskRequestDTO
 import com.lucas.lptasks.service.TaskService
 import com.lucas.lptasks.utils.ApiResponse
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -27,4 +28,8 @@ class TaskController(
     fun saveTask(
         @RequestBody taskData: TaskRequestDTO,
     ): ApiResponse<out Any?> = taskService.saveTask(taskData)
+    fun saveTasks(
+        @RequestBody tasksToSave: List<TaskRequestDTO>,
+    ): ApiResponse<out Any?> = taskService.saveTask(tasksToSave)
+
 }
