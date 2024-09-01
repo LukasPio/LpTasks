1733cee4a3a76ee02a86ca5ccb177d21fcc37049
LukasPio <lucaspio.galvao@gmail.com>
2024-09-01 00:01:52 -0300

    feat(DTO): add NotEmpty validation to all TaskRequestDTO camps

--- src/main/kotlin/com/lucas/lptasks/dto/TaskRequestDTO.kt
+++ src/main/kotlin/com/lucas/lptasks/dto/TaskRequestDTO.kt
@@ -1,12 +1,17 @@
 package com.lucas.lptasks.dto
 
 import com.lucas.lptasks.model.Task
+import jakarta.validation.constraints.NotEmpty
 import java.util.UUID
 
 data class TaskRequestDTO(
+    @field:NotEmpty(message = "Title is required")
     val title: String,
+    @field:NotEmpty(message = "Description is required")
     val description: String,
+    @field:NotEmpty(message = "Category is required")
     val category: String,
+    @field:NotEmpty(message = "Priority is required")
     val priority: String,
 ) {
     fun toTask(): Task =
