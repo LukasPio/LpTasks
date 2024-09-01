9092e5ece622389074c514ac7fc21a63739ecaf3
LukasPio <lucaspio.galvao@gmail.com>
2024-09-01 00:02:17 -0300

    refactor(service): remove all unnecessary try/catch

--- src/main/kotlin/com/lucas/lptasks/service/TaskService.kt
+++ src/main/kotlin/com/lucas/lptasks/service/TaskService.kt
@@ -1,9 +1,9 @@
 package com.lucas.lptasks.service
 
 import com.lucas.lptasks.dto.TaskRequestDTO
+import com.lucas.lptasks.model.Task
 import com.lucas.lptasks.repository.TaskRepository
 import com.lucas.lptasks.utils.ApiResponse
-import org.springframework.dao.DataAccessException
 import org.springframework.stereotype.Service
 import java.util.UUID
 
@@ -21,36 +21,42 @@ class TaskService(
     }
 
     fun getTaskById(id: String): ApiResponse<out Any?> {
-        try {
-            val task = taskRepository.findById(UUID.fromString(id))
-            if (task.isEmpty) {
-                return ApiResponse.noTaskFoundById(id)
-            }
-            return ApiResponse.success(task.get().toDTO(), "Successfully retrieved task: ${task.get().id}")
-        } catch (e: IllegalArgumentException) {
-            return ApiResponse.invalidUUID()
+        val task = taskRepository.findById(UUID.fromString(id))
+        return if (task.isEmpty) {
+            ApiResponse.noTaskFoundById(id)
+        } else {
+            ApiResponse.success(task.get().toDTO(), "Successfully retrieved task: ${task.get().id}")
         }
     }
 
-    fun saveTask(taskData: List<TaskRequestDTO>): ApiResponse<out Any?> =
-        try {
-            taskData.forEach { (taskRepository.save(it.toTask())) }
-            ApiResponse.created("Successfully saved all tasks")
-        } catch (e: DataAccessException) {
-            ApiResponse.internalError("Internal error while saving task")
-        }
+    fun saveTask(taskData: List<TaskRequestDTO>): ApiResponse<out Any?> {
+        taskData.forEach { (taskRepository.save(it.toTask())) }
+        return ApiResponse.created("Successfully saved all tasks")
+    }
 
     fun deleteTask(id: String): ApiResponse<out Any?> {
-        try {
-            val uuid = UUID.fromString(id)
-            val existsTask: Boolean = taskRepository.existsById(uuid)
-            if (existsTask) {
-                taskRepository.deleteById(uuid)
-                return ApiResponse.success(null, "Successfully deleted task: $id")
-            }
-            return ApiResponse.noTaskFoundById(id)
-        } catch (e: IllegalArgumentException) {
-            return ApiResponse.invalidUUID()
+        val uuid = UUID.fromString(id)
+        val existsTask: Boolean = taskRepository.existsById(uuid)
+        return if (existsTask) {
+            taskRepository.deleteById(uuid)
+            ApiResponse.success(null, "Successfully deleted task: $id")
+        } else {
+            ApiResponse.noTaskFoundById(id)
         }
+    }
+
+    fun updateTask(
+        updateData: TaskRequestDTO,
+        id: String,
+    ): ApiResponse<out Any?> {
+        val task = taskRepository.findById(UUID.fromString(id))
+        if (task.isEmpty) {
+            return ApiResponse.noTaskFoundById(id)
+        }
+        val taskToSave: Task = task.get()
+        taskToSave.update(updateData)
+        taskRepository.save(taskToSave)
+
+        return ApiResponse.noContent("Successfully updated task: $id")
     }
 }
