49ea7eebcbf743ca58f0e0be52ce6427f66b1d2e
LukasPio <lucaspio.galvao@gmail.com>
2024-09-01 00:01:19 -0300

    feat(controller): create put route to update tasks

--- src/main/kotlin/com/lucas/lptasks/controller/TaskController.kt
+++ src/main/kotlin/com/lucas/lptasks/controller/TaskController.kt
@@ -3,15 +3,20 @@ package com.lucas.lptasks.controller
 import com.lucas.lptasks.dto.TaskRequestDTO
 import com.lucas.lptasks.service.TaskService
 import com.lucas.lptasks.utils.ApiResponse
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotEmpty
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Validated
 @RequestMapping("/app/tasks")
 class TaskController(
     private val taskService: TaskService,
@@ -26,8 +31,14 @@ class TaskController(
 
     @PostMapping
     fun saveTasks(
-        @RequestBody tasksToSave: List<TaskRequestDTO>,
+        @Valid @NotEmpty @RequestBody tasksToSave: List<TaskRequestDTO>,
     ): ApiResponse<out Any?> = taskService.saveTask(tasksToSave)
+
+    @PutMapping("/{id}")
+    fun updateTasks(
+        @RequestBody updateData: TaskRequestDTO,
+        @PathVariable id: String,
+    ): ApiResponse<out Any?> = taskService.updateTask(updateData, id)
 
     @DeleteMapping("/{id}")
     fun deleteTask(
