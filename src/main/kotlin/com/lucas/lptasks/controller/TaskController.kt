432c44fb06cc05d361d62f8f7151cc3991179e7d
LukasPio <lucaspio.galvao@gmail.com>
2024-09-02 00:42:34 -0300

    refactor(controller): segregate function of return responses to controller and only logic to service

--- src/main/kotlin/com/lucas/lptasks/controller/TaskController.kt
+++ src/main/kotlin/com/lucas/lptasks/controller/TaskController.kt
@@ -1,8 +1,11 @@
 package com.lucas.lptasks.controller
 
 import com.lucas.lptasks.dto.TaskRequestDTO
+import com.lucas.lptasks.dto.TaskResponseDTO
+import com.lucas.lptasks.service.ResponseService
 import com.lucas.lptasks.service.TaskService
 import com.lucas.lptasks.utils.ApiResponse
+import jakarta.transaction.Transactional
 import jakarta.validation.Valid
 import jakarta.validation.constraints.NotEmpty
 import org.springframework.validation.annotation.Validated
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -20,28 +24,53 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/app/tasks")
 class TaskController(
     private val taskService: TaskService,
+    private val responseService: ResponseService,
 ) {
     @GetMapping
-    fun getAllTasks(): ApiResponse<out Any?> = taskService.getAllTasks()
+    fun getAllTasks(): ApiResponse<List<TaskResponseDTO>> {
+        val tasks = taskService.getAllTasks()
+        return responseService.fetchedAllTasks(tasks)
+    }
 
-    @GetMapping("/{id}")
+    @GetMapping("/title")
+    fun getTaskByName(
+        @Valid @NotEmpty @RequestParam taskTitle: String,
+    ): ApiResponse<List<TaskResponseDTO>> {
+        val tasks = taskService.getTasksByName(taskTitle)
+        return responseService.fetchedAllTasks(tasks)
+    }
+
+    @GetMapping("/id")
     fun getTaskById(
-        @PathVariable id: String,
-    ): ApiResponse<out Any?> = taskService.getTaskById(id)
+        @Valid @NotEmpty @RequestParam id: String,
+    ): ApiResponse<TaskResponseDTO> {
+        val task = taskService.getTaskById(id)
+        return responseService.success(task, "Successfully got task")
+    }
 
     @PostMapping
     fun saveTasks(
         @Valid @NotEmpty @RequestBody tasksToSave: List<TaskRequestDTO>,
-    ): ApiResponse<out Any?> = taskService.saveTasks(tasksToSave)
+    ): ApiResponse<Unit> {
+        taskService.saveTasks(tasksToSave)
+        return responseService.noContent("Tasks saved successfully")
+    }
 
     @PutMapping("/{id}")
     fun updateTasks(
-        @RequestBody updateData: TaskRequestDTO,
-        @PathVariable id: String,
-    ): ApiResponse<out Any?> = taskService.updateTask(updateData, id)
+        @Valid @NotEmpty @RequestBody updateData: TaskRequestDTO,
+        @Valid @NotEmpty @PathVariable id: String,
+    ): ApiResponse<TaskResponseDTO> {
+        val task = taskService.updateTask(updateData, id)
+        return responseService.success(task, "Successfully updated task")
+    }
 
     @DeleteMapping("/{id}")
+    @Transactional
     fun deleteTask(
-        @PathVariable id: String,
-    ): ApiResponse<out Any?> = taskService.deleteTask(id)
+        @Valid @NotEmpty @PathVariable id: String,
+    ): ApiResponse<Unit> {
+        taskService.deleteTask(id)
+        return responseService.noContent("Task deleted successfully")
+    }
 }
--- src/main/kotlin/com/lucas/lptasks/service/TaskService.kt
+++ src/main/kotlin/com/lucas/lptasks/service/TaskService.kt
@@ -1,9 +1,12 @@
 package com.lucas.lptasks.service
 
 import com.lucas.lptasks.dto.TaskRequestDTO
+import com.lucas.lptasks.dto.TaskResponseDTO
+import com.lucas.lptasks.exception.InvalidTaskPriorityException
+import com.lucas.lptasks.exception.TaskNotFoundException
 import com.lucas.lptasks.model.Task
 import com.lucas.lptasks.repository.TaskRepository
-import com.lucas.lptasks.utils.ApiResponse
+import com.lucas.lptasks.utils.DataValidator
 import org.springframework.stereotype.Service
 import java.util.UUID
 
@@ -11,52 +14,55 @@ import java.util.UUID
 class TaskService(
     private val taskRepository: TaskRepository,
 ) {
-    fun getAllTasks(): ApiResponse<out Any?> {
+    fun getAllTasks(): List<TaskResponseDTO> {
         val tasks = taskRepository.findAll().map { it.toDTO() }
-        return if (tasks.isEmpty()) {
-            ApiResponse.notFound("No tasks found")
-        } else {
-            ApiResponse.success(tasks, "Successfully retrieved ${tasks.size} tasks")
-        }
+        return tasks
     }
 
-    fun getTaskById(id: String): ApiResponse<out Any?> {
+    fun getTaskById(id: String): TaskResponseDTO {
         val task = taskRepository.findById(UUID.fromString(id))
-        return if (task.isEmpty) {
-            ApiResponse.noTaskFoundById(id)
-        } else {
-            ApiResponse.success(task.get().toDTO(), "Successfully retrieved task: ${task.get().id}")
+        if (task.isEmpty) {
+            throw TaskNotFoundException()
+        }
+        return task.get().toDTO()
+    }
+
+    fun saveTasks(taskData: List<TaskRequestDTO>) {
+        taskData.forEach {
+            if (DataValidator.isValidPriority(it.priority)) {
+                taskRepository.save(it.toTask())
+            } else {
+                throw InvalidTaskPriorityException()
+            }
         }
     }
 
-    fun saveTasks(taskData: List<TaskRequestDTO>): ApiResponse<out Any?> {
-        taskData.forEach { (taskRepository.save(it.toTask())) }
-        return ApiResponse.created("Successfully saved all tasks")
-    }
-
-    fun deleteTask(id: String): ApiResponse<out Any?> {
+    fun deleteTask(id: String) {
         val uuid = UUID.fromString(id)
         val existsTask: Boolean = taskRepository.existsById(uuid)
-        return if (existsTask) {
-            taskRepository.deleteById(uuid)
-            ApiResponse.success(null, "Successfully deleted task: $id")
-        } else {
-            ApiResponse.noTaskFoundById(id)
-        }
+        if (!existsTask) throw TaskNotFoundException()
+
+        taskRepository.deleteById(uuid)
     }
 
     fun updateTask(
         updateData: TaskRequestDTO,
         id: String,
-    ): ApiResponse<out Any?> {
-        val task = taskRepository.findById(UUID.fromString(id))
-        if (task.isEmpty) {
-            return ApiResponse.noTaskFoundById(id)
-        }
-        val taskToSave: Task = task.get()
-        taskToSave.update(updateData)
-        taskRepository.save(taskToSave)
+    ): TaskResponseDTO {
+        val optionalTask = taskRepository.findById(UUID.fromString(id))
 
-        return ApiResponse.noContent("Successfully updated task: $id")
+        if (optionalTask.isEmpty) throw TaskNotFoundException()
+        if (!DataValidator.isValidPriority(updateData.priority)) throw InvalidTaskPriorityException()
+
+        val task: Task = optionalTask.get()
+        task.update(updateData)
+        taskRepository.save(task)
+        return task.toDTO()
+    }
+
+    fun getTasksByName(taskTitle: String): List<TaskResponseDTO> {
+        val tasks = taskRepository.findByTitle(taskTitle)
+        if (tasks.isEmpty()) throw TaskNotFoundException()
+        return tasks.map { it.toDTO() }
     }
 }
