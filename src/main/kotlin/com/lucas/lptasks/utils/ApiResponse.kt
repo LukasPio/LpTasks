999ad1f017ad307263d3f998fbdeff4d03eb10e9
LukasPio <lucaspio.galvao@gmail.com>
2024-09-02 00:38:00 -0300

    refactor(apiResponse): separate apiResponse to apiResponse create functions

--- src/main/kotlin/com/lucas/lptasks/utils/ApiResponse.kt
+++ src/main/kotlin/com/lucas/lptasks/utils/ApiResponse.kt
@@ -4,27 +4,4 @@ data class ApiResponse<T>(
     val body: T?,
     val message: String,
     val statusCode: Int,
-) {
-    companion object {
-        fun <T> success(
-            body: T,
-            message: String,
-        ): ApiResponse<T> = ApiResponse(body, message, 200)
-
-        fun created(message: String): ApiResponse<Unit> = ApiResponse(Unit, message, 201)
-
-        fun noContent(message: String): ApiResponse<Unit> = ApiResponse(Unit, message, 204)
-
-        fun notFound(message: String): ApiResponse<Unit> = ApiResponse(Unit, message, 404)
-
-        fun internalError(message: String): ApiResponse<Unit> = ApiResponse(Unit, message, 500)
-
-        fun unauthorized(message: String): ApiResponse<Unit> = ApiResponse(Unit, message, 401)
-
-        fun badRequest(message: String): ApiResponse<Unit> = ApiResponse(Unit, message, 400)
-
-        fun invalidUUID(): ApiResponse<Unit> = badRequest("Invalid UUID")
-
-        fun noTaskFoundById(id: String): ApiResponse<Unit> = badRequest("No task found with id: $id")
-    }
-}
+)
