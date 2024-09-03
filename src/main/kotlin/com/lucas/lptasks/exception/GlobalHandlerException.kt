e32448d73e52a006f76414963bf02c0fd96ff451
LukasPio <lucaspio.galvao@gmail.com>
2024-09-02 22:14:46 -0300

    feat(exception): add exceptionHandlers to new exceptions

--- src/main/kotlin/com/lucas/lptasks/exception/GlobalHandlerException.kt
+++ src/main/kotlin/com/lucas/lptasks/exception/GlobalHandlerException.kt
@@ -4,6 +4,7 @@ import com.lucas.lptasks.service.ResponseService
 import jakarta.validation.ConstraintViolationException
 import org.springframework.dao.DataAccessException
 import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.orm.jpa.JpaSystemException
 import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -18,19 +19,19 @@ class GlobalHandlerException(
     protected fun noResourceFound() = responseService.notFound("Invalid url provided")
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
-    protected fun httpRequestMethodNotSupported() = responseService.badRequest("The request method is not supported on actual url.")
+    protected fun httpRequestMethodNotSupported() = responseService.badRequest("The request method is not supported on actual url")
 
     @ExceptionHandler(HttpMessageNotReadableException::class)
-    protected fun httpMessageNotReadable() = responseService.badRequest("Non-existent params were provided on request body.")
+    protected fun httpMessageNotReadable() = responseService.badRequest("Non-existent params were provided on request body")
 
     @ExceptionHandler(MissingServletRequestParameterException::class)
     protected fun missingServletRequestParameter() = responseService.badRequest("Insufficient request params were provided")
 
     @ExceptionHandler(ConstraintViolationException::class)
-    protected fun validationExceptions() = responseService.badRequest("All parameters on request body are required.")
+    protected fun validationExceptions() = responseService.badRequest("All parameters on request body are required")
 
     @ExceptionHandler(IllegalArgumentException::class)
-    protected fun illegalArgument() = responseService.badRequest("Invalid values were provided.")
+    protected fun illegalArgument() = responseService.badRequest("Invalid parameters were provided")
 
     @ExceptionHandler(DataAccessException::class)
     protected fun dataAccess() = responseService.internalError("An error occurred with saving/getting on database")
@@ -38,6 +39,15 @@ class GlobalHandlerException(
     @ExceptionHandler(TaskNotFoundException::class)
     protected fun taskNotFound() = responseService.taskNotFound()
 
+    @ExceptionHandler(InvalidOrderSortException::class)
+    protected fun invalidOrderSort() = responseService.badRequest("Invalid order sort provided")
+
+    @ExceptionHandler(InvalidTaskCategoryException::class)
+    protected fun invalidTaskCategory() = responseService.badRequest("Invalid task category provided")
+
     @ExceptionHandler(InvalidTaskPriorityException::class)
-    protected fun invalidTaskPriority() = responseService.badRequest("Invalid priority provided")
+    protected fun invalidTaskPriority() = responseService.badRequest("Invalid task priority provided")
+
+    @ExceptionHandler(JpaSystemException::class)
+    protected fun jpaSystem() = responseService.internalError("An internal error occurred, try again later")
 }
